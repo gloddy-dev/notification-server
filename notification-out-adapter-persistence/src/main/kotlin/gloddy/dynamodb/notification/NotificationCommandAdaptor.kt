3e39463ea58d187b6f828c0ccd11e374d372e1bf
twoosky <lhjae3@naver.com>
2023-11-06 21:59:12 +0900

    feat: notification 저장 command service 구현

---
+++ notification-out-adapter-persistence/src/main/kotlin/gloddy/dynamodb/notification/NotificationCommandAdaptor.kt
@@ -0,0 +1,15 @@
+package gloddy.dynamodb.notification
+
+import gloddy.notification.Notification
+import gloddy.notification.port.out.NotificationCreatePort
+import org.springframework.stereotype.Component
+
+@Component
+class NotificationCommandAdaptor(
+    private val notificationRepository: NotificationRepository,
+    private val notificationMapper: NotificationMapper
+): NotificationCreatePort {
+    override fun save(notification: Notification) {
+        notificationRepository.save(notificationMapper.toEntity(notification))
+    }
+}
