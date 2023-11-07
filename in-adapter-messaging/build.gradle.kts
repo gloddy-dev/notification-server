41ec946f0e376773806fd3df36776dde607c6446
jihwan2da <wlghks8982@naver.com>
2023-11-07 20:56:23 +0900

    chore: in-adapter-messaging 모듈에 application, domain 모듈 의존 설정

--- in-adapter-messaging/build.gradle.kts
+++ in-adapter-messaging/build.gradle.kts
@@ -7,6 +7,8 @@ bootJar.enabled = false
 jar.enabled = true
 
 dependencies {
+    implementation(project(":notification-application"))
+    implementation(project(":notification-domain"))
     //aws sqs
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:3.0.0"))
