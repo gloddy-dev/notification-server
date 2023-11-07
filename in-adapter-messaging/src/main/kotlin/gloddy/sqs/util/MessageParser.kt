9daff347c156d138328387b544779e4deba8481b
jihwan2da <wlghks8982@naver.com>
2023-11-07 20:30:43 +0900

    refactor: 불필요한 let 함수 제거

--- in-adapter-messaging/src/main/kotlin/gloddy/sqs/util/MessageParser.kt
+++ in-adapter-messaging/src/main/kotlin/gloddy/sqs/util/MessageParser.kt
@@ -1,26 +1,22 @@
 package gloddy.sqs.util
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import gloddy.payload.apply.ApplyEvent
-import gloddy.payload.group.GroupMemberEvent
+import gloddy.payload.apply.ApplyPayload
+import gloddy.payload.group.GroupMemberPayload
 
 class MessageParser {
 
     companion object{
         private val objectMapper = jacksonObjectMapper()
 
-        fun parseApplyEvent(message: String): ApplyEvent {
+        fun parseApplyEvent(message: String): ApplyPayload {
             val payload = parsePayloadFromMessage(message)
-            return payload.let {
-                objectMapper.readValue(it, ApplyEvent::class.java)
-            }
+            return objectMapper.readValue(payload, ApplyPayload::class.java)
         }
 
-        fun parseGroupMemberEvent(message: String): GroupMemberEvent {
+        fun parseGroupMemberEvent(message: String): GroupMemberPayload {
             val payload = parsePayloadFromMessage(message)
-            return payload.let {
-                objectMapper.readValue(it, GroupMemberEvent::class.java)
-            }
+            return objectMapper.readValue(payload, GroupMemberPayload::class.java)
         }
 
         private fun parsePayloadFromMessage(message: String): String {
