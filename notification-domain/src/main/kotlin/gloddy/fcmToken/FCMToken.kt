c39e36d929abbf08184fb492956cac3832a9f034
twoosky <lhjae3@naver.com>
2023-11-07 17:02:41 +0900

    fix: notification 도메인 필드명 수정 (redirectId)

--- notification-domain/src/main/kotlin/gloddy/fcmToken/FCMToken.kt
+++ notification-domain/src/main/kotlin/gloddy/fcmToken/FCMToken.kt
@@ -4,6 +4,7 @@ import gloddy.notification.UserId
 
 @JvmInline
 value class FirebaseToken(val value: String)
+
 data class FCMToken(
     val userId: UserId,
     val token: FirebaseToken
--- notification-domain/src/main/kotlin/gloddy/notification/Notification.kt
+++ notification-domain/src/main/kotlin/gloddy/notification/Notification.kt
@@ -1,12 +1,12 @@
 package gloddy.notification
 
-@JvmInline
-value class GroupId(val value: Long)
+
 @JvmInline
 value class UserId(val value: Long)
+
 data class Notification(
     val userId: UserId,
-    val groupId: GroupId,
+    val redirectId: Long,
     val content: String,
     val type: NotificationType
 )
