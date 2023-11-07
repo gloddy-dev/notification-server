7873714dcba2b6cad38165accb5c0d8745ac96e9
twoosky <lhjae3@naver.com>
2023-11-07 17:03:18 +0900

    refactor: token 조회 및 payload 생성 로직 push 모듈로 분리

--- push/src/main/kotlin/gloddy/NotificationPushService.kt
+++ push/src/main/kotlin/gloddy/NotificationPushService.kt
@@ -1,24 +1,44 @@
 package gloddy
 
+import gloddy.dynamodb.fcmToken.FCMTokenQueryAdapter
+import gloddy.fcmToken.FirebaseToken
+import gloddy.notification.NotificationType
 import gloddy.notification.event.NotificationPushEvent
 import org.springframework.stereotype.Component
 
 @Component
 class NotificationPushService(
-    private val pushClient: PushClient
+    private val pushClient: PushClient,
+    private val fcmTokenQueryAdapter: FCMTokenQueryAdapter
 ) {
     companion object {
         const val NOTIFICATION_TITLE = "Gloddy"
     }
 
     fun push(event: NotificationPushEvent) {
+        if (!event.type.isNotificationRequired) {
+            return
+        }
+
+        val fcmToken = fcmTokenQueryAdapter.get(event.userId)
+        send(fcmToken.token, event)
+    }
+
+    fun send(token: FirebaseToken, event: NotificationPushEvent) {
         pushClient.push(
             PushCommand(
-                token = event.token,
+                token = token.value,
                 title = NOTIFICATION_TITLE,
                 content = event.content,
-                payload = event.payload
+                payload = createPayload(event.redirectId, event.type)
             )
         )
+    }
+
+    private fun createPayload(redirectId: Long, type: NotificationType): Map<String, String> {
+        return buildMap {
+            put("redirectId", redirectId.toString())
+            put("type", type.name)
+        }
     }
 }
