4f886a44a8c323650eec03283a0aa020146c8365
jihwan2da <wlghks8982@naver.com>
2023-11-09 23:48:53 +0900

    feat:GroupNotificationCreate 서비스 구현(그룹 게시글, 상태)

--- notification-application/src/main/kotlin/gloddy/notification/port/in/GroupNotificationCreateUseCase.kt
+++ notification-application/src/main/kotlin/gloddy/notification/port/in/GroupNotificationCreateUseCase.kt
@@ -1,7 +1,11 @@
 package gloddy.notification.port.`in`
 
+import gloddy.notification.dto.GroupArticleEvent
 import gloddy.notification.dto.GroupEvent
+import gloddy.notification.dto.GroupStatusEvent
 
 interface GroupNotificationCreateUseCase {
     fun create(groupEvent: GroupEvent)
+    fun create(groupStatusEvent: GroupStatusEvent)
+    fun create(groupArticleEvent: GroupArticleEvent)
 }
--- notification-domain/src/main/kotlin/gloddy/notification/NotificationType.kt
+++ notification-domain/src/main/kotlin/gloddy/notification/NotificationType.kt
@@ -7,7 +7,17 @@ enum class NotificationType(
     APPLY_APPROVE("지원하신 모임이 승인되었어요! 모임 고지를 확인해주세요", true),
     APPLY_REFUSE("지원하신 모임이 거절되었어요. 다른 모임을 찾아볼까요?", false),
     APPLY_CREATE("새로운 모임 지원서가 도착했어요!", true),
-    GROUP_LEAVE("님이 그룹에서 나갔어요", true);
+    GROUP_LEAVE("님이 그룹에서 나갔어요", true),
+    GROUP_ARTICLE_CREATE("새로운 게시글을 확인해주세요", true),
+    GROUP_APPROACHING_START("""
+        1시간 뒤 모임이 시작돼요!
+        준비는 되었나요?
+    """.trimIndent(), true),
+    GROUP_END("""
+        모임은 즐거우셨나요?
+        함께 했던 인원들을 평가해주세요!
+    """.trimIndent(), false)
+    ;
 
     companion object {
         fun of(type: String): NotificationType {
