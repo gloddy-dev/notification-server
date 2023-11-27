d13c4cec407511543699d89243d4e864f41d8b0c
twoosky <lhjae3@naver.com>
2023-11-27 22:07:06 +0900

    feat: notificaiton 도메인에 id 필드 추가

--- notification-domain/src/main/kotlin/gloddy/notification/Notification.kt
+++ notification-domain/src/main/kotlin/gloddy/notification/Notification.kt
@@ -1,12 +1,19 @@
 package gloddy.notification
 
+import java.util.*
+
 
 @JvmInline
 value class UserId(val value: Long)
 
+@JvmInline
+value class NotificationId(val value: String)
+
 data class Notification(
+    val id: NotificationId = NotificationId(UUID.randomUUID().toString()),
     val userId: UserId,
     val redirectId: Long,
+    val title: String,
     val content: String,
     val type: NotificationType
 )
