b8041960b1ada85deec43fe787c8f600284e5d43
twoosky <lhjae3@naver.com>
2023-12-01 02:40:19 +0900

    fix: 애플리케이션 레벨 스레드 풀 설정 제거

--- push/src/main/kotlin/gloddy/config/AsyncConfiguration.kt
+++ push/src/main/kotlin/gloddy/config/AsyncConfiguration.kt
@@ -2,26 +2,24 @@ package gloddy.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import java.util.concurrent.Executor
 
 @Configuration
 @EnableAsync
-class AsyncConfiguration : AsyncConfigurer {
+class AsyncConfiguration {
 
     companion object {
-        private const val PUSH_THREAD_NAME_PREFIX = "PUSH-ASYNC-THREAD-"
+        private const val PUSH_THREAD_NAME_PREFIX = "PUSH-THREAD-"
     }
 
-    @Bean(name = ["PUSH-EVENT-ASYNC-EXECUTOR"])
-    override fun getAsyncExecutor(): Executor {
+    @Bean(name = ["pushThreadPoolTaskExecutor"])
+    fun pushThreadPoolTaskExecutor(): Executor {
         val executor = ThreadPoolTaskExecutor()
         executor.setThreadNamePrefix(PUSH_THREAD_NAME_PREFIX)
-        executor.corePoolSize = 10
-        executor.maxPoolSize = 10
-        executor.initialize()
+        executor.corePoolSize = 9
+        executor.maxPoolSize = 9
         return executor
     }
 }
