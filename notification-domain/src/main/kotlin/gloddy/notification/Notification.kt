cd50c18179a26f5685661c8afc8fe2ec7ffc32ef
twoosky <lhjae3@naver.com>
2023-12-06 06:33:04 +0900

    feat: redirectId에 대한 value 객체 정의

--- notification-domain/src/main/kotlin/gloddy/notification/Notification.kt
+++ notification-domain/src/main/kotlin/gloddy/notification/Notification.kt
@@ -7,12 +7,15 @@ import java.util.*
 value class UserId(val value: Long)
 
 @JvmInline
+value class RedirectId(val value: Long)
+
+@JvmInline
 value class NotificationId(val value: String)
 
 data class Notification(
     val id: NotificationId = NotificationId(UUID.randomUUID().toString()),
     val userId: UserId,
-    val redirectId: Long,
+    val redirectId: RedirectId,
     val title: String,
     val content: String,
     val type: NotificationType
