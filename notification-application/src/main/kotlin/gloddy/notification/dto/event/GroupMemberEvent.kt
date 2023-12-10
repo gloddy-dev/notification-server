6cb69d03e424a1f7dac6e6cf5188a386bc1572ea
jihwan2da <wlghks8982@naver.com>
2023-12-10 16:04:46 +0900

    fix: Domain GroupMemberEvent 변경(groupMemberId -> groupId, userId)

--- notification-application/src/main/kotlin/gloddy/notification/dto/event/GroupMemberEvent.kt
+++ notification-application/src/main/kotlin/gloddy/notification/dto/event/GroupMemberEvent.kt
@@ -1,10 +1,12 @@
 package gloddy.notification.dto.event
 
+import gloddy.notification.UserId
 import gloddy.notification.dto.event.eventType.GroupMemberEventType
 import java.time.LocalDateTime
 
 data class GroupMemberEvent(
-    val groupMemberId: Long,
+    val userId: UserId,
+    val groupId: Long,
     val eventType: GroupMemberEventType,
     val eventDateTime: LocalDateTime
 )
